355c742dce992094e96ba361076b771e0ce19d7b
nianod <arnoldkk422@gmail.com>
2025-05-04 17:56:59 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+import Amazon from './components/amazon'
+
+function App () {
+  return(
+    <>
+      <h1>Yooh goodnight</h1>
+      <Amazon/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/amazon.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import './amazon.css'
+
+function Amazon () {
+    return(
+        <div className="amazon">
+            <p>Let's Start coding right away</p>
+        </div>
+    )
+}
+
+export default Amazon
