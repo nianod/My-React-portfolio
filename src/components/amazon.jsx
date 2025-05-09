c2df55857d6b4f86c18f68d8b4b6c5fb595f64ca
nianod <arnoldkk422@gmail.com>
2025-05-09 23:39:17 +0300

    day 1

--- src/components/amazon.jsx
+++ src/components/amazon.jsx
@@ -3,13 +3,15 @@ import "./amazon.css"
 
 const Amazon = () => {
   return (
-    <div className='Navbar'>
-        <h1>Welcome to Arnold's Tech</h1>
-        <ul className='navlist'>
-            <li>About</li>
-            <li>Contact</li>
-            <li>Home</li>
-        </ul>     
+    <div className='Nav'>
+        <div className='Navbar'>
+            <h1>Welcome to Arnold's Tech</h1>
+            <ul className='navlist'>
+                <li>About</li>
+                <li>Contact</li>
+                <li>Home</li>
+            </ul>     
+        </div>
     </div>
   )
 }
