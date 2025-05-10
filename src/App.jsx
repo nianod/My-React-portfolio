ca7c626161c5c84cf3ba77e36cfb5380f7887844
nianod <arnoldkk422@gmail.com>
2025-05-10 17:27:17 +0300

    second day

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,1 @@
-import React from 'react'
-import Amazon from './components/amazon'
-import Twos from './components/twos'
-
-const App = () => {
-  return (
-    <div>
-      <Amazon/>
-      <Twos name = "Arnold"  />
-    </div>
-  )
-}
-
-export default App
+ 
