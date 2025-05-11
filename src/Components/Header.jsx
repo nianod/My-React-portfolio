d8fbd9b78037ffe6652bc1ccd1e99cd22d3aba8e
nianod <arnoldkk422@gmail.com>
2025-05-11 21:28:36 +0300

    special

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,16 +1,27 @@
 import React from 'react'
 import './header.css'
 
-
+// const navContent = [
+//     {label: "Home", to: "/", icon: Home}
+// ]
 const Header = () => {
   return (
     <div className='div'>
        <div className='profile-pic'>
-            <img src="" alt="myself" />
-            <h2>Arnold</h2>
+            <img src="/arnold.png" alt="myself" />
+            <Push />
+            <Push/>
        </div>
     </div>
   )
+}
+
+function Push() {
+  return (
+    <div>
+    <h2>Welcome to my pop</h2>
+    </div>
+  )  
 }
 
 export default Header
