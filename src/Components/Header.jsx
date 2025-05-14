7d84059a97f4221aee003e6feb46674f9b4c8847
nianod <arnoldkk422@gmail.com>
2025-05-14 21:53:21 +0300

    nav

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,6 +1,5 @@
-import React from 'react'
-import './header.css'
-
+import React from "react";
+import "./header.css";
 
 // const navContent = [
 //     {label: "Home", to: "/", icon: Home}
@@ -8,72 +7,65 @@ import './header.css'
 
 const user = {
   name: "Arnold",
-  imageURL: '3c4813fa70d7f0597c00220179dceb9f.png',
+  imageURL: "3c4813fa70d7f0597c00220179dceb9f.png",
   imageSize: 50,
-}
+};
 
-function Profile () {
-  return(
+function Profile() {
+  return (
     <>
-    <h1>{user.name}</h1>
-      <img 
-        className='ownerimage'
+      <h1>{user.name}</h1>
+      <img
+        className="ownerimage"
         src={user.imageURL}
-        alt = {"photo of "+ user.name}
+        alt={"photo of " + user.name}
         style={{
           width: user.imageSize,
-          height: user.imageSize
+          height: user.imageSize,
         }}
-      
       />
     </>
-  )
+  );
 }
 
- export default Profile
+export default Profile;
 
+const navContents = [
+  { label: "Home" },
+  { label: "Skills" },
+  { label: "Education" },
+  { label: "Blogs" },
+  { label: "Projects" },
+  { label: "Contact" },
+];
 
- const navContents = [
-  {label: "Home"},
-  {label: "Skills"},
-  {label: "Education"},
-  {label: "Blogs"},
-  {label: "Projects"},
-  {label: "Contact"}
-]
-
-function NavC () {
-  return(
+function NavC() {
+  return (
     <div>
       <nav>
-        {navContents.map((item, index) =>(
-          <a key={index} href={`#${item.label.toLocaleLowerCase()}`} >
+        {navContents.map((item, index) => (
+          <a key={index} href={`#${item.label.toLocaleLowerCase()}`}>
             {item.label}
           </a>
         ))}
       </nav>
     </div>
-  )
+  );
 }
 
-export { NavC }
-
-
-
+export { NavC };
 
 // function Push() {
 //   const clicks = (event) => {
 //     alert("clicked")
 //     event.preventdefault()
 //     console.log("correct");
-    
+
 //   }
 //   return (
 //     <div>
 //     <h2>Welcome to my pop</h2>
 //     <button onClick={clicks }>try me</button>
 //     </div>
-//   )  
+//   )
 // }
-
-
