4d0712415db1d21795474a911f5788c30fab50d6
nianod <arnoldkk422@gmail.com>
2025-05-16 21:05:37 +0300

    foot commit

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,6 +1,6 @@
 import { FaGithub, FaLinkedin, FaWhatsapp, FaTwitter, FaInstagram , FaDiscord} from "react-icons/fa"
 const FooterContents = [
-    { icon: <FaGithub/> },
+    { icon: <FaGithub/>, URL: "https://github.com/nianod" },
     { icon: <FaLinkedin/> },
     { icon: <FaWhatsapp/> },
     { icon: <FaTwitter/> },
@@ -11,9 +11,9 @@ const FooterContents = [
 const Footer = () => {
     return (
         <>
-        <div className="icons">
-            {FooterContents.map((index, title) => (
-                <a key={title} >{index} </a>
+        <div className="footerUIcons">
+            {FooterContents.map((item, index) => (
+                <a key={index} href={item.URL} target="_blank">{item.icon} </a>
             ))}
         </div>
         </>
