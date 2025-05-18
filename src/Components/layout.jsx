11e4a853131c6754303e90673036edfd52708e66
nianod <arnoldkk422@gmail.com>
2025-05-18 16:34:22 +0300

    page blogs

--- src/Components/layout.jsx
+++ src/Components/layout.jsx
@@ -1,11 +1,14 @@
+import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import Footer from "./Footer";
 
-export default function Layout ({ children }) {
+export default function Layout () {
     return (
         <>
         <Header/>
-        <main className="p-20"> {children} </main>
+            <main className="p-20"> 
+                <Outlet/>
+            </main>
         <Footer/>
         </>
     )
