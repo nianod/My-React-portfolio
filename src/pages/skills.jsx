114d9af4a83a85158efbfc6dfe0e2189005b7dce
nianod <arnoldkk422@gmail.com>
2025-05-20 18:43:08 +0300

    home complete

--- src/pages/skills.jsx
+++ src/pages/skills.jsx
@@ -11,9 +11,30 @@ const Skills = () => {
       <h1 className='text-blue-400 font-bold flex justify-center items-center text-xl underline'> Self-Taught-Skills</h1>
       <p className='EXP text-white'> {skillTitle.titl} </p>
     </div>
-    <div>
-      <h1>Programming Languages</h1>
-      <div> {languages.Js}  </div>
+    <div className='flex flex-col items-center'>
+      <h1 className='text-blue-400 underline font-bold text-xl'>Programming Languages</h1>
+      <div className='flex flex-col ityems-center'>
+        <img className='iconImg' src={icons.jS} 
+         alt={languages.Js} />
+        <h3>{languages.Js} </h3> 
+      </div>
+      <div>
+        <img className='iconImg' src={icons.py} 
+         alt={languages.Py} />
+        <h3>{languages.Py} </h3> 
+      </div>
+      <div>
+        <img className='iconImg' src={icons.ts} 
+         alt={languages.Ts} />
+        <h3>{languages.Ts} </h3> 
+      </div>
+      <div>
+        <img className='iconImg' src={icons.html}
+         alt={languages.HtM} />
+        <img className='iconImg' src={icons.css}
+         alt={languages.HtM} />
+        <h3>{languages.HtM} </h3> 
+      </div>
     </div>
   </>  
   )
