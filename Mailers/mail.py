ae3427331fa89462d340308f55bb47dc453905b4
nianod <arnoldkk422@gmail.com>
2025-10-17 19:56:29 +0300

    mail server initiation

--- Mailers/mail.py
+++ Mailers/mail.py
@@ -32,6 +32,23 @@ Phone_No: {params.phoneNoPlaceholder}
 Message: {params.messagePlaceholder}
 """
 
+message = MINEMultipart()
+message['from'] = sender_email
+message['to'] = receiver_email
+message['subject'] = subject
+message.attach(MIMEtext(body, "plain"))
+
+try:
+    with smtplib.SMTP('smtp.gmail.com', 587) as server:
+        server.starttls()
+        server.login(sender_email, sender_password)
+        server.send_message(message)
+
+        {"message" : "Email was send"}
+    expect Exception as e:
+
+    raise HTTPException(status_code=500, detail=str(e))
+
 @app.get("/")
 def contact():
     return {"Hello": "Arnold, I saw you portfolio. Looks wow"}
