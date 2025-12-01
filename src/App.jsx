92ced13c0d69beb27ef7b873fecb163fb319b2c6
nianod <arnoldkk422@gmail.com>
2025-12-01 20:52:21 +0300

    animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,42 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { useEffect } from 'react'
-import AOS from "aos"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
+import AOS from "aos";
 import "aos/dist/aos.css";
-import Home from './pages/home'
-import Skills from './pages/skills'
-import Education from './pages/education'
-import Blogs from './pages/blogs'
-import Projects from './pages/projects'
-import Contact from './pages/contact'
-import Layout from './Components/layout'
+import Home from "./pages/home";
+import Skills from "./pages/skills";
+import Education from "./pages/education";
+import Blogs from "./pages/blogs";
+import Projects from "./pages/projects";
+import Contact from "./pages/contact";
+import Layout from "./Components/layout";
+import ParticleBackground from "./ParticleBackground";
 
 const App = () => {
   useEffect(() => {
     AOS.init({
       duration: 1000,
       once: true,
-    })
+    });
   }, []);
-  return(
+  return (
     <Router>
-      <Routes> 
-        <Route path="/" element={<Layout />}>
-          <Route index element = {<Home />} /> 
-          <Route path="skills" element={<Skills />} />
-          <Route path="education" element={<Education />} />
-          <Route path="blogs" element={<Blogs />} />
-          <Route path="projects" element={<Projects />} />
-          <Route path="contact" element={<Contact />} />
-        </Route>
-      </Routes>
+      <div className="relative min-h-screen">
+        <ParticleBackground />
+        <div className="relative z-10">
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<Home />} />
+              <Route path="skills" element={<Skills />} />
+              <Route path="education" element={<Education />} />
+              <Route path="blogs" element={<Blogs />} />
+              <Route path="projects" element={<Projects />} />
+              <Route path="contact" element={<Contact />} />
+            </Route>
+          </Routes>
+        </div>
+      </div>
     </Router>
-  )
-}
+  );
+};
 
-export default App
+export default App;
